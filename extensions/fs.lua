e11c725c59ba19d0a752efc113a42b25eff7795a
Daniel Brady <daniel.brady@tapjoy.com>
2018-03-11 14:22:10 -0400

    Add convenience function for loading dir

--- extensions/fs.lua
+++ extensions/fs.lua
@@ -1,4 +1,5 @@
 local fs_ext = {}
+require 'extensions/string'
 
 -- Returns a list of directories in the given path.
 function fs_ext.dirs(path)
@@ -18,6 +19,28 @@ function fs_ext.dirs(path)
   directoryContents:close()
 
   return directories
+end
+
+-- Load each .LUA file in a given directory, with any arguments given.
+function fs_ext.loadAllScripts(rootDir, ...)
+  -- Make sure our root ends with a directory marker.
+  rootDir = rootDir:endsWith('/') and rootDir or rootDir..'/'
+
+  local loadedScripts = {}
+  local _,scripts = hs.fs.dir(rootDir)
+  repeat
+    local filename = scripts:next()
+    if filename and filename ~= '.' and filename ~= '..' then
+      print('\t\tloading script: '..filename)
+      -- Load the script, passing the given arguments as parameters to the Lua chunk.
+      -- Using `assert(loadfile(...))` instead of `require` to be compatible with Spoons.
+      local script = assert(loadfile(rootDir..filename))(...)
+      local basename = filename:match("^(.+)%.") -- Matches everything up to the first '.'
+      loadedScripts[basename] = script
+    end
+  until filename == nil
+
+  return loadedScripts
 end
 
 ------
