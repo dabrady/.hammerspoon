6299de7771e4906b7a0c57b32ce68f50459d1ddf
Daniel Brady <daniel.brady@tapjoy.com>
2020-08-25 16:04:44 -0400

    bugfix(ConfigWatcher): Stub out `stop` API

--- scripts/config_watcher.lua
+++ scripts/config_watcher.lua
@@ -3,6 +3,11 @@ reloadConfiguration:bindHotkeys({
   reloadConfiguration = {{'cmd', 'alt', 'ctrl'}, 'R'}
 })
 
+-- Annoyingly, this spoon has as `start` without a `stop`, so I'm adding one to keep with the expectations of
+-- a 'watcher' API.
+function reloadConfiguration:stop()
+  return nil
+end
 ------
 
 return reloadConfiguration
